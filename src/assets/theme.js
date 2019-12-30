373b3e4561eb5f0aca5bb299eb6f9832a1e97d60
dtangerfors <54536270+dtangerfors@users.noreply.github.com>
2019-12-30 12:12:56 +0100

    Initial asset files

---
+++ src/assets/theme.js
@@ -0,0 +1,9 @@
+const theme = {
+    primaryColor: '#0F5224',
+    secondaryColor: '#F4F4ED',
+    titleFont: 'NeueMachina',
+    bodyFont: 'Aileron',
+    defaultFontSize: '1.4rem'
+}
+
+export default theme
