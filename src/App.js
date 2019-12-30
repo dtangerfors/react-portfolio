7f06295ac8948be41d37312e49655a7cc8d26291
dtangerfors <54536270+dtangerfors@users.noreply.github.com>
2019-12-30 12:17:27 +0100

    Inital first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Nav from "./layout/Nav.js";
+import Hero from './layout/Hero.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav></Nav>
+      <Hero></Hero>
     </div>
   );
 }
