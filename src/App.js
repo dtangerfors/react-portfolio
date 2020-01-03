d656ea382438bc5cd64211fedfe008a4fef00406
dtangerfors <54536270+dtangerfors@users.noreply.github.com>
2020-01-03 16:53:36 +0100

    Moved ThemeProvider and added new components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { ThemeProvider } from 'styled-components'
+import theme from './assets/theme'
 
-import Nav from "./layout/Nav.js";
-import Hero from './layout/Hero.js'
+import Nav from "./layout/Nav"
+import Hero from './layout/Hero'
+import Cases from './layout/Cases'
+import Footer from './layout/Footer'
 
 function App() {
   return (
     <div className="App">
-      <Nav></Nav>
-      <Hero></Hero>
+      <ThemeProvider theme={theme}>
+        <Nav />
+        <Hero />
+        <Cases />
+        <Footer />
+      </ThemeProvider>
     </div>
   );
 }
