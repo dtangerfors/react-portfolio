4f17cb7e4577273f709fe12ec7cb02083d56b142
dtangerfors <54536270+dtangerfors@users.noreply.github.com>
2020-01-03 16:47:36 +0100

    Array with cases

---
+++ src/cases.js
@@ -0,0 +1,25 @@
+const cases = [
+    {
+        id: 1,
+        title: "Diamond Service",
+        subTitle: "A new visual identity",
+        preview: "assets/img/diamond-logotyp-visitkort.jpg",
+        bigDisplay: true
+    },
+    {
+        id: 2,
+        title: "Önabybornas",
+        subTitle: "Branding for a chocolate company",
+        preview: "assets/img/onaby-1.jpg",
+        bigDisplay: false
+    },
+    {
+        id: 3,
+        title: "Ut & Njut",
+        subTitle: "A new visual identity",
+        preview: "assets/img/utnjut-landscape-logo.jpg",
+        bigDisplay: false
+    }
+]
+
+export default cases
