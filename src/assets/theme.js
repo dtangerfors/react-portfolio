c2c28475c4ddecb87e2c8389260a35f4579e247a
dtangerfors <54536270+dtangerfors@users.noreply.github.com>
2020-01-03 16:52:03 +0100

    Added new variables

--- src/assets/theme.js
+++ src/assets/theme.js
@@ -3,7 +3,8 @@ const theme = {
     secondaryColor: '#F4F4ED',
     titleFont: 'NeueMachina',
     bodyFont: 'Aileron',
-    defaultFontSize: '1.4rem'
+    defaultFontSize: '1.4rem',
+    defaultWidth: '85vw'
 }
 
 export default theme
