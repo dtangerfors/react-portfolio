37fd25f3657a31f3ea156eb758a88435d49b3f7a
dtangerfors <54536270+dtangerfors@users.noreply.github.com>
2020-01-05 12:34:11 +0100

    Object containing resources for resource page

---
+++ src/resource-list.js
@@ -0,0 +1,58 @@
+const RESOURCE_LIST = {
+    images: [
+        {
+            title: 'Unsplash',
+            desc: 'Over 1 million free high-resolution images brought to you by the world’s most generous community of photographers.',
+            url: 'https://unsplash.com/'
+        },
+        {
+            title: 'Pexels',
+            desc: 'Pexels provides high quality and completely free stock photos',
+            url: 'https://pexels.com/'
+        },
+        {
+            title: 'unDraw',
+            desc: 'Open-source illustrations for every project you can imagine and create.',
+            url: 'https://undraw.co/'
+        }
+
+    ],
+    typography: [
+        {
+            title: 'Pangram Pangram',
+            desc: 'Over 1 million free high-resolution images brought to you by the world’s most generous community of photographers.',
+            url: 'https://unsplash.com/'
+        },
+        {
+            title: 'Google Fonts',
+            desc: 'Pexels provides high quality and completely free stock photos',
+            url: 'https://pexels.com/'
+        },
+        {
+            title: 'Atipo',
+            desc: 'Open-source illustrations for every project you can imagine and create.',
+            url: 'https://undraw.co/'
+        }
+
+    ],
+    devlopment: [
+        {
+            title: 'Emmet Cheat sheet',
+            desc: 'Cheat sheet that helps you boost your productivity',
+            url: 'https://docs.emmet.io/cheat-sheet/'
+        },
+        {
+            title: 'Write better CSS',
+            desc: 'Pexels provides high quality and completely free stock photos',
+            url: 'https://pexels.com/'
+        },
+        {
+            title: 'BEM',
+            desc: 'Open-source illustrations for every project you can imagine and create.',
+            url: 'https://undraw.co/'
+        }
+
+    ]
+}
+
+export default RESOURCE_LIST
