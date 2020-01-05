ab5e3a6124ccae673b679ed7613426e94a08c0ba
dtangerfors <54536270+dtangerfors@users.noreply.github.com>
2020-01-05 12:35:26 +0100

    Added new variable

--- src/assets/theme.js
+++ src/assets/theme.js
@@ -4,7 +4,8 @@ const theme = {
     titleFont: 'NeueMachina',
     bodyFont: 'Aileron',
     defaultFontSize: '1.4rem',
-    defaultWidth: '85vw'
+    defaultWidth: '85vw',
+    mobileWidth: 'calc(100% - 4rem)'
 }
 
 export default theme
