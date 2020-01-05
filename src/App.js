dc2f411953368bcd9451b26215c0e463fa3b4d27
dtangerfors <54536270+dtangerfors@users.noreply.github.com>
2020-01-05 12:42:45 +0100

    Added react-router and new components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css'
 import { ThemeProvider } from 'styled-components'
 import theme from './assets/theme'
 
-import Nav from "./layout/Nav"
-import Hero from './layout/Hero'
-import Cases from './layout/Cases'
-import Footer from './layout/Footer'
+import Nav from "./components/layout/Nav"
+import Footer from './components/layout/Footer'
+
+import Home from './pages/Home'
+import Resources from './pages/Resources'
 
 function App() {
   return (
     <div className="App">
-      <ThemeProvider theme={theme}>
-        <Nav />
-        <Hero />
-        <Cases />
-        <Footer />
-      </ThemeProvider>
+      <Router>
+        <ThemeProvider theme={theme}>
+          <Nav />
+          <Route exact path="/" component={Home} />
+          <Route path="/resources" component={Resources} />
+          <Footer />
+        </ThemeProvider>
+      </Router>
     </div>
   );
 }
