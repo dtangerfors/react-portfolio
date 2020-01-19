a88f1ade65355bc63b4bc67e8b92fed2111ee00a
dtangerfors <54536270+dtangerfors@users.noreply.github.com>
2020-01-19 23:25:44 +0100

    Updated list with new resources

--- src/resource-list.js
+++ src/resource-list.js
@@ -2,17 +2,17 @@ const RESOURCE_LIST = {
     images: [
         {
             title: 'Unsplash',
-            desc: 'Over 1 million free high-resolution images brought to you by the world’s most generous community of photographers.',
+            desc: 'My favorite source of free high quality images from photographers around the world.',
             url: 'https://unsplash.com/'
         },
         {
             title: 'Pexels',
-            desc: 'Pexels provides high quality and completely free stock photos',
+            desc: 'Another great source of free high quality images which also lets you directly choose what size you want your image to be.',
             url: 'https://pexels.com/'
         },
         {
             title: 'unDraw',
-            desc: 'Open-source illustrations for every project you can imagine and create.',
+            desc: 'Great looking open-source illustrations with possibility to choose your own main color.',
             url: 'https://undraw.co/'
         }
 
@@ -20,36 +20,49 @@ const RESOURCE_LIST = {
     typography: [
         {
             title: 'Pangram Pangram',
-            desc: 'Over 1 million free high-resolution images brought to you by the world’s most generous community of photographers.',
-            url: 'https://unsplash.com/'
+            desc: 'A small type foundry with well designed typefaces that also lets you try them for free in your projects.',
+            url: 'https://pangrampangram.com/'
         },
         {
             title: 'Google Fonts',
-            desc: 'Pexels provides high quality and completely free stock photos',
-            url: 'https://pexels.com/'
-        },
-        {
-            title: 'Atipo',
-            desc: 'Open-source illustrations for every project you can imagine and create.',
-            url: 'https://undraw.co/'
+            desc: 'Does it need any further presentation; free web fonts for your projects.',
+            url: 'https://fonts.google.com/'
         }
 
     ],
     devlopment: [
         {
             title: 'Emmet Cheat sheet',
-            desc: 'Cheat sheet that helps you boost your productivity',
+            desc: 'Cheat sheet that helps you boost your productivity in coding.',
             url: 'https://docs.emmet.io/cheat-sheet/'
         },
         {
             title: 'Write better CSS',
-            desc: 'Pexels provides high quality and completely free stock photos',
-            url: 'https://pexels.com/'
+            desc: 'Useful article that shows how to write better, smarter and more re-usable code.',
+            url: 'https://paulcpederson.com/articles/css-for-people-who-hate-css/'
         },
         {
             title: 'BEM',
-            desc: 'Open-source illustrations for every project you can imagine and create.',
-            url: 'https://undraw.co/'
+            desc: 'Block Element Modifer – methodology that helps you to create reusable components and code sharing in front-end development.',
+            url: 'http://getbem.com/'
+        }
+
+    ],
+    inspiration: [
+        {
+            title: 'Pinterest',
+            desc: "Get online here and you're stuck scrolling through pins forever – great site finding inspiration for anything you wish for.",
+            url: 'https://pinterest.com/'
+        },
+        {
+            title: 'Brand New',
+            desc: 'Website covering redesigns and new designs of notable products, companies, services, and organizations.',
+            url: 'https://www.underconsideration.com/brandnew/'
+        },
+        {
+            title: 'Awwwards',
+            desc: 'Great source for inspiration in web design and code. UI/UX, Interaction, Animations, Menus, Contact pages – they got it.',
+            url: 'https://www.awwwards.com/'
         }
 
     ]
