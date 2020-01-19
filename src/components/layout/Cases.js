a22f7bbdf414c57ed75b5bf7442cdf5c5c246fae
dtangerfors <54536270+dtangerfors@users.noreply.github.com>
2020-01-19 23:29:59 +0100

    Updated with new design

--- src/components/layout/Cases.js
+++ src/components/layout/Cases.js
@@ -10,14 +10,27 @@ import cases from '../../cases'
 const CASE_SECTION = styled.section`
     display: flex;
     flex-direction: row;
+    justify-content: space-between;
     flex-wrap: wrap;
-    margin: 0 1rem;
+    width: ${props => props.theme.defaultWidth};
+
+    @media ${device.phone} { 
+       width: ${props => props.theme.mobileWidth};
+    }
 `;
 
 const CASE_ARTICLE = styled.article`
-    width: ${props => props.bigDisplay ? "100%" : "50%"};
-    padding: 0 1rem;
+    width: ${props => props.bigDisplay ? "100%" : "calc(50% - 1rem)"};
+    padding: 2rem;
     margin-bottom: 3rem;
+    cursor: pointer;
+
+    border: 1px solid ${props => props.theme.primaryColor};
+    
+    &:hover {
+        box-shadow: .5rem .5rem 0 0 ${props => props.theme.primaryColor};
+        transform: translateY(-.5rem) translateX(-.5rem);
+    }
 
     @media ${device.phone} { 
        width: 100%;
@@ -25,7 +38,14 @@ const CASE_ARTICLE = styled.article`
 `;
 
 const CASE__INFO = styled.div`
+    margin-bottom: 2rem;
+`;
+
+const CASE__TITLE = styled(H2)`
+    padding-bottom: 1rem;
     margin-bottom: 1rem;
+    border-bottom: 1px solid ${props => props.theme.primaryColor};
+
 `;
 
 const CASE__IMG = styled.img`
@@ -45,6 +65,24 @@ export default class Cases extends Component {
                 {cases.map(item =>
                     <CASE_ARTICLE bigDisplay={item.bigDisplay} key={item.id}>
                         <CASE__INFO>
+                            <CASE__TITLE>{item.title}</CASE__TITLE>
+                            <H4>{item.subTitle}</H4>
+                        </CASE__INFO>
+                        <CASE__IMG src={item.preview} alt={item.title}></CASE__IMG>
+                    </CASE_ARTICLE>
+                )}
+            </CASE_SECTION>
+        )
+    }
+}
+
+/* export default class Cases extends Component {
+    render() {
+        return (
+            <CASE_SECTION>
+                {cases.map(item =>
+                    <CASE_ARTICLE bigDisplay={item.bigDisplay} key={item.id}>
+                        <CASE__INFO>
                             <H2>{item.title}</H2>
                             <H4>{item.subTitle}</H4>
                         </CASE__INFO>
@@ -55,3 +93,4 @@ export default class Cases extends Component {
         )
     }
 }
+ */
