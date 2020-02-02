ceff701463a67a95014833d2e9777effee97a6bf
dtangerfors <54536270+dtangerfors@users.noreply.github.com>
2020-02-02 22:54:09 +0100

    Updated App to a class component, added FooterNav

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import {
   BrowserRouter as Router,
   Route,
@@ -8,26 +8,32 @@ import { ThemeProvider } from 'styled-components'
 import theme from './assets/theme'
 
 import Nav from "./components/layout/Nav"
-import Footer from './components/layout/Footer'
+import FooterNav from './components/FooterNav'
 
 import Home from './pages/Home'
 import Resources from './pages/Resources'
 import About from './pages/About'
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <ThemeProvider theme={theme}>
-          <Nav />
-          <Route exact path="/" component={Home} />
-          <Route path="/resources" component={Resources} />
-          <Route path="/about" component={About} />
-          <Footer />
-        </ThemeProvider>
-      </Router>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+
+
+
+    return (
+      <div className="App">
+        <Router>
+          <ThemeProvider theme={theme}>
+            <Nav />
+            <Route exact path="/" component={Home} />
+            <Route path="/resources" component={Resources} />
+            <Route path="/about" component={About} />
+            <FooterNav />
+          </ThemeProvider>
+        </Router>
+      </div>
+    )
+  }
 }
 
 export default App;
