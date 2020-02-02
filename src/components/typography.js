5be03c98c717d063b10efcd8f84e6fdf72c12dd0
dtangerfors <54536270+dtangerfors@users.noreply.github.com>
2020-02-02 22:55:09 +0100

    Added line-height and media queries

--- src/components/typography.js
+++ src/components/typography.js
@@ -4,6 +4,7 @@ import device from '../assets/breakpoints'
 
 export const H1 = styled.h1`
     font-size: 7rem;
+    line-height: 1.3em;
     font-family: ${props => props.theme.titleFont};
     font-weight: 400;
     color: ${props => props.theme.primaryColor};
@@ -15,13 +16,19 @@ export const H1 = styled.h1`
 
 export const H2 = styled.h2`
     font-size: 4.5rem;
+    line-height: 1.3em;
     font-family: ${props => props.theme.titleFont};
     font-weight: 400;
     color: ${props => props.theme.primaryColor};
+
+    @media ${device.tabPort} { 
+        font-size: 3rem;
+    }
 `;
 
 export const H3 = styled.h3`
     font-size: 2.5rem;
+    line-height: 1.3em;
     font-family: ${props => props.theme.titleFont};
     font-weight: 400;
     color: ${props => props.theme.primaryColor};
@@ -29,6 +36,7 @@ export const H3 = styled.h3`
 
 export const H4 = styled.h4`
     font-size: ${props => props.theme.defaultFontSize};
+    line-height: 1.3em;
     font-family: ${props => props.theme.bodyFont};
     font-weight: 400;
     color: ${props => props.theme.primaryColor};
@@ -41,17 +49,19 @@ export const A = styled.a`
     color: ${props => props.theme.primaryColor};
     text-decoration: ${props => props.underline ? 'underline' : 'none'};
 
-    margin-top: ${props => props.footer ? '2rem' : 0};
+    margin-top: ${props => props.footer ? '1.5rem' : 0};
 
     &:hover {
         text-decoration: ${props => props.underline ? 'none' : 'underline'};
-
     }
 `;
 
 export const EXTERNAL_LINK = styled(A)`
     &::after {
         content: ' \u2197';
+        text-decoration: none;
+        display: inline-block;
+        margin-left: 5px;
     }
 `;
 
