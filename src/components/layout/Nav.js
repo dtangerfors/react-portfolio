d35fcecb5635dcf2d0311c6f1895ca6251af0639
dtangerfors <54536270+dtangerfors@users.noreply.github.com>
2020-02-02 22:59:15 +0100

    Added exports for some styled components

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
+
 import logo from '../../assets/logo.svg'
 
-import styled from 'styled-components';
-import theme from '../../assets/theme';
+import styled from 'styled-components'
 
 import device from '../../assets/breakpoints'
-import HamburgerMenu from 'react-hamburger-menu';
 
 // NAV STYLING – DESKTOP 
 
@@ -28,7 +27,7 @@ const NAV = styled.nav`
     }
 `;
 
-const NAV__LOGO_BOX = styled.div`
+export const NAV__LOGO_BOX = styled.div`
     width: 5rem;
     height: 5rem;
     border-right: 1px solid ${props => props.theme.primaryColor};
@@ -38,7 +37,7 @@ const NAV__LOGO_BOX = styled.div`
     }
 `;
 
-const NAV__LOGO = styled.img`
+export const NAV__LOGO = styled.img`
     width: 100%;
     padding: 1rem;
     box-sizing: border-box;
@@ -81,11 +80,13 @@ const NAV__LIST_ITEM = styled.li`
     }
 `;
 
-const NAV__LINK = styled(NavLink)`
+export const NAV__LINK = styled(NavLink)`
     font-family: ${props => props.theme.titleFont};
     font-size: ${props => props.theme.defaultFontSize};
     color: ${props => props.theme.primaryColor};
     text-decoration: none;
+
+    margin-top: ${props => props.footer ? '1.5rem' : 0};
 
     &:hover {
         text-decoration: underline;
@@ -95,7 +96,6 @@ const NAV__LINK = styled(NavLink)`
 const NAV__NAME_BOX = styled.div`
     border-left: 1px solid ${props => props.theme.primaryColor};
     display: flex;
-    /* display: ${window.innerWidth < 1000 ? "none" : "flex"}; */
     align-items: center;
     right: 0;
     position: absolute;
@@ -107,158 +107,45 @@ const NAV__NAME_BOX = styled.div`
 `;
 
 const NAV_NAME = styled.span`
+    margin: 0 2rem;
+
     font-family: ${props => props.theme.titleFont};
     font-size: 2.5rem;
+
     color: ${props => props.theme.primaryColor};
     text-transform: uppercase;
-
-    margin: 0 2rem;
 `;
 
 // NAV STYLING – MOBILE
 
-const MOBILE_NAV_WRAPPER = styled.nav`
-    width: calc((${props => props.theme.mobileWidth}) - 4rem);
-
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: space-between;
-    margin: 0 auto;
-
-    border: 1px solid ${props => props.theme.primaryColor};
-    background: ${props => props.theme.secondaryColor};
-
-    position: fixed;
-    bottom: 4rem;
-    z-index: 10;
-`;
-
-
-const MOBILE__LINK_BOX = styled.ul`
-    width: 100%;
-
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    flex-wrap: wrap;
-
-    margin: 0;
-    padding: 0;
-
-    list-style-type: none;
-`;
-
-const MOBILE__LIST_ITEM = styled.li`
-    width: 100%;
-    padding: 2rem 3rem;
-    border-bottom: 1px solid ${props => props.theme.primaryColor};
-`;
-
-const MOBILE__LINK = styled(NavLink)`
-    width: 100%;
-    font-family: ${props => props.theme.titleFont};
-    font-size: ${props => props.theme.defaultFontSize};
-    color: ${props => props.theme.primaryColor};
-    text-decoration: none;
-    
-`;
-
-const HAMBURGER__WRAPPER = styled.div`
-    margin-right:2rem;
-    align-self: center;
-`;
-
-class NavDesktop extends Component {
-    render() {
-        return (
-            <NAV>
-                <NAV__LOGO_BOX>
-                    <NAV__LINK to="/">
-                        <NAV__LOGO src={logo} alt="Dtangerfors" />
-                    </NAV__LINK>
-                </NAV__LOGO_BOX>
-                <NAV__LINK_BOX>
-                    <NAV__LIST_ITEM><NAV__LINK to="/resources">Frontend</NAV__LINK></NAV__LIST_ITEM>
-                    <NAV__LIST_ITEM><NAV__LINK to="/resources">Design</NAV__LINK></NAV__LIST_ITEM>
-                    <NAV__LIST_ITEM>
-                        <NAV__LINK to="/resources">Resources</NAV__LINK>
-                    </NAV__LIST_ITEM>
-                    <NAV__LIST_ITEM><NAV__LINK to="/about">About me</NAV__LINK></NAV__LIST_ITEM>
-                    <NAV__LIST_ITEM><NAV__LINK to="/resources">Contact</NAV__LINK></NAV__LIST_ITEM>
-                </NAV__LINK_BOX>
-                <NAV__NAME_BOX>
-                    <NAV_NAME>Daniel Tängerfors</NAV_NAME>
-                </NAV__NAME_BOX>
-            </NAV>
-        )
-    }
-}
-
-class NavMobile extends Component {
-
-    constructor() {
-        super();
-        this.state = {
-            open: false,
-            hideOrShowHamburgerDropDown: 'nav'
-        }
-    }
-
-    handleClick() {
-        this.setState({
-            open: !this.state.open
-        });
-    }
-    render() {
-        return (
-            <MOBILE_NAV_WRAPPER>
-                <MOBILE__LINK_BOX>
-                    <MOBILE__LIST_ITEM><MOBILE__LINK to="/resources">Frontend</MOBILE__LINK></MOBILE__LIST_ITEM>
-                    <MOBILE__LIST_ITEM><MOBILE__LINK to="/resources">Design</MOBILE__LINK></MOBILE__LIST_ITEM>
-                    <MOBILE__LIST_ITEM>
-                        <MOBILE__LINK to="/resources">Resources</MOBILE__LINK>
-                    </MOBILE__LIST_ITEM>
-                    <MOBILE__LIST_ITEM><MOBILE__LINK to="/about">About me</MOBILE__LINK></MOBILE__LIST_ITEM>
-                    <MOBILE__LIST_ITEM><MOBILE__LINK to="/resources">Contact</MOBILE__LINK></MOBILE__LIST_ITEM>
-                </MOBILE__LINK_BOX>
-                <NAV__LOGO_BOX>
-                    <NAV__LINK to="/">
-                        <NAV__LOGO src={logo} alt="Dtangerfors" />
-                    </NAV__LINK>
-                </NAV__LOGO_BOX>
-                <HAMBURGER__WRAPPER>
-                    <HamburgerMenu
-                        isOpen={this.state.open}
-                        menuClicked={this.handleClick.bind(this)}
-                        width={30}
-                        height={20}
-                        strokeWidth={1}
-                        color={theme.primaryColor}
-                        animationDuration={0.5}
-                    />
-                </HAMBURGER__WRAPPER>
-            </MOBILE_NAV_WRAPPER>
-        );
-    }
-}
 
 export default class Nav extends Component {
-
     render() {
 
         const isMobile = window.orientation > -1;
 
-        if (isMobile) {
+        if (!isMobile) {
             return (
-                <NavMobile />
-            );
+                <NAV>
+                    <NAV__LOGO_BOX>
+                        <NAV__LINK to="/">
+                            <NAV__LOGO src={logo} alt="Dtangerfors" />
+                        </NAV__LINK>
+                    </NAV__LOGO_BOX>
+                    <NAV__LINK_BOX>
+                        <NAV__LIST_ITEM>
+                            <NAV__LINK to="/resources">Resources</NAV__LINK>
+                        </NAV__LIST_ITEM>
+                        <NAV__LIST_ITEM><NAV__LINK to="/about">About me</NAV__LINK></NAV__LIST_ITEM>
+                    </NAV__LINK_BOX>
+                    <NAV__NAME_BOX>
+                        <NAV_NAME>Daniel Tängerfors</NAV_NAME>
+                    </NAV__NAME_BOX>
+                </NAV>
+            )
         } else {
-            return (
-                <NavDesktop />
-            );
+            return (null)
         }
-
     }
 }
 
